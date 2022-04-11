49c4d94b3dd28ca6e38761dcbe53adf81a87de51
luthfeew <luthfeew@gmail.com>
2022-04-11 17:01:54 +0700

    Thread

---
+++ AplikasiNonThread_1198/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikasiNonThread_1198
+{
+    internal class Program
+    {
+        public class non_Thread
+        {
+            public void cuci_baju()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Cuci baju ke {0}", i);
+                }
+            }
+
+            public void setrika()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("Setrika ke {0}", i);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Mulai melakukan pekerjaan");
+            Console.WriteLine("=========================");
+
+            non_Thread tid1 = new non_Thread();
+
+            tid1.cuci_baju();
+            tid1.setrika();
+
+            Console.WriteLine("=========================");
+            Console.WriteLine("Selesai......!");
+            Console.WriteLine("=========================");
+            Console.ReadLine();
+        }
+    }
+}
