33f12518ce579242e0fc7081e471f8ab2ee4aca3
luthfeew <luthfeew@gmail.com>
2022-04-11 16:55:36 +0700

    Thread

---
+++ AplikasiThread_1198/MyThread.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AplikasiThread_1198
+{
+    internal class MyThread
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Cuci baju ke {0}", i);
+                Thread.Sleep(3000);
+            }
+        }
+
+        public static void Thread2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Setrika ke {0}", i);
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
---
+++ AplikasiThread_1198/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AplikasiThread_1198
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("Mulai melakukan pekerjaan");
+            Console.WriteLine("=========================");
+
+            Thread tid1 = new Thread(new ThreadStart(MyThread.Thread1));
+            Thread tid2 = new Thread(new ThreadStart(MyThread.Thread2));
+
+            tid1.Start();
+            tid2.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
