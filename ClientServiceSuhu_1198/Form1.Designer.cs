5dede34ff019a16630f6d8d7814150d33a3d832c
luthfeew <luthfeew@gmail.com>
2022-04-15 10:48:23 +0700

    design

--- ClientServiceSuhu_1198/Form1.Designer.cs
+++ ClientServiceSuhu_1198/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtF = new System.Windows.Forms.TextBox();
+            this.txtK = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Celcius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reamur";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fahrenheit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Kelvin";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Konversi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(136, 54);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 22);
+            this.txtC.TabIndex = 5;
+            // 
+            // txtR
+            // 
+            this.txtR.Location = new System.Drawing.Point(136, 200);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(100, 22);
+            this.txtR.TabIndex = 6;
+            // 
+            // txtF
+            // 
+            this.txtF.Location = new System.Drawing.Point(136, 267);
+            this.txtF.Name = "txtF";
+            this.txtF.Size = new System.Drawing.Size(100, 22);
+            this.txtF.TabIndex = 7;
+            // 
+            // txtK
+            // 
+            this.txtK.Location = new System.Drawing.Point(136, 335);
+            this.txtK.Name = "txtK";
+            this.txtK.Size = new System.Drawing.Size(100, 22);
+            this.txtK.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(316, 411);
+            this.Controls.Add(this.txtK);
+            this.Controls.Add(this.txtF);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Konversi Suhu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtF;
+        private System.Windows.Forms.TextBox txtK;
     }
 }
 
